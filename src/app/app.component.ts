1759a0f6370968bb04240da9f40caae5669551f7
jamila EL ABBASI <jamilaelabbasi@gmail.com>
2024-11-24 13:03:03 +0100

    create all of components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {DashBoardComponent} from './components/dash-board/dash-board.component';
+import {ContactComponent} from './components/contact/contact.component';
+import {DeliciousBakingComponent} from './components/delicious-baking/delicious-baking.component';
+import {OurBreadsComponent} from './components/our-breads/our-breads.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DashBoardComponent, ContactComponent, DeliciousBakingComponent, OurBreadsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
