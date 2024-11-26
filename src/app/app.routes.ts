b806c104f6cea6d20bf634b71e9df73f75385e19
jamilaELABBASI <jamilaelabbasi@gmail.com>
2024-11-26 16:11:31 +0100

    ajouter le routage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {DashBoardComponent} from './components/dash-board/dash-board.component';
+import {OurBreadsComponent} from './components/our-breads/our-breads.component';
+import {DeliciousBakingComponent} from './components/delicious-baking/delicious-baking.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'',component:DashBoardComponent},
+  {path:'delicious_baking',component:DeliciousBakingComponent},
+  {path:'ours_breads',component:OurBreadsComponent},
+
+
+
+];
