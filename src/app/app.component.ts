a39b5d30f04f46091745e76a9c94f60968bedb79
jamila EL ABBASI <jamilaelabbasi@gmail.com>
2024-11-28 17:18:44 +0100

    ajouter le routage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,35 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {DashBoardComponent} from './components/dash-board/dash-board.component';
 import {DeliciousBakingComponent} from './components/delicious-baking/delicious-baking.component';
 import {OurBreadsComponent} from './components/our-breads/our-breads.component';
 
+import { Router, NavigationEnd } from '@angular/router';  // <-- Ensure correct imports
+import { filter } from 'rxjs/operators';
+import {CommonModule} from '@angular/common';
+import {FormBuilder, ReactiveFormsModule} from '@angular/forms';  // <-- This is necessary for the filter operator
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, DashBoardComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, DashBoardComponent,CommonModule, DeliciousBakingComponent, OurBreadsComponent,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'boulangerie';
+  showDashboard: boolean = true;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    // Subscribe to router events to detect route changes
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd) // Trigger on NavigationEnd events
+    ).subscribe((event: NavigationEnd) => {
+      // Check if the current route is 'register'
+      this.showDashboard = event.url !== '/register' && event.url!=='/connect' // Hide dashboard if the route is 'register'
+    });
+  }
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,14 @@ import { Routes } from '@angular/router';
 import {DashBoardComponent} from './components/dash-board/dash-board.component';
 import {OurBreadsComponent} from './components/our-breads/our-breads.component';
 import {DeliciousBakingComponent} from './components/delicious-baking/delicious-baking.component';
+import {RegisterComponent} from './components/register/register.component';
+import {ConnectComponent} from './components/connect/connect.component';
 
 export const routes: Routes = [
-  {path:'',component:DashBoardComponent},
   {path:'delicious_baking',component:DeliciousBakingComponent},
   {path:'ours_breads',component:OurBreadsComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'connect',component:ConnectComponent},
 
 
 
